210671938a4473258b069587fb1324952c811bda
sermalo16 <sersaml17@gmail.com>
2022-03-03 18:55:30 -0600

    Creando Carpeta React

---
+++ medexp5000/medexfronted/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Title} from './Components/UX/Title';
+
+function App() {
+  return (
+  <div>
+    <Title>Hola Mundo Cruel</Title>
+    <Title>Hola</Title>
+  </div>  );
+}
+
+export default App;
